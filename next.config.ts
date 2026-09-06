ea84f921352b6dfbd2adcc148273e30734b48838
tkmaher <tkmaher@umich.edu>
2026-09-06 08:57:26 -0500

    testing

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-    output: 'export', // Tells Next.js to export static HTML files
-    basePath: '/fonts-index', // Replace with your exact GitHub repo name
+    output: 'export',
+    basePath: '', 
     images: {
-      unoptimized: true, // Required because Next.js default image optimization needs a server
+      unoptimized: true, 
     },
 };
 
